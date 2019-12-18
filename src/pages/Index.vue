22c8a01f03479672307d091f7ee2c149fe622644
Anton S. Stetsenko <anton.stetsenko@ssa-outsourcing.com>
2019-12-18 16:15:33 +0200

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <Layout>
+
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+
+    <h1>Hello, world!</h1>
+
+    <p>{{test}}</p>
+
+    <p class="home-links">
+      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
+      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+    </p>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+
+  metaInfo: {
+    title: 'Hello, world!'
+  },
+  data() {
+    return {
+      test: 'Lorem ipsum dolor sit amet, consectetur'
+    }
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
